02c7c6e38f7090bf13d236b541ed9ab179207d07
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-10 12:12:43 +0530

    Update tshirts_test.js
    comments have been added

--- tshirts_test.js
+++ tshirts_test.js
@@ -6,7 +6,8 @@ import('chai').then(chai => {
     expect(size(37)).to.equal('S');
     expect(size(40)).to.equal('M');
     expect(size(43)).to.equal('L');
-    expect(size(38)).to.equal('S');
+    // The expected output is 'S',but actual output is 'L'.The test case fails
+    expect(size(38)).to.equal('S');  
 
     console.log('All is well (maybe!)');
 });
