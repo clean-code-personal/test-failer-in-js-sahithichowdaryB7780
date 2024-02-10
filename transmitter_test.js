7441afa358b3a08dc9caa2293e3b9e261cb657cb
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-10 15:58:46 +0530

    Update transmitter_test.js

--- transmitter_test.js
+++ transmitter_test.js
@@ -1,6 +1,5 @@
 const { transmitInCelcius, transmissionFailureCount } = require('./transmitter');
-import('chai').then(chai => {
-    const { expect } = chai;
+const { expect }=require('chai');
 
     // Transmit temperatures
     transmitInCelcius(410.4);
@@ -12,17 +11,10 @@ import('chai').then(chai => {
     console.log(`Transmission failed ${transmissionFailureCount} times.`);
 
     // Test if transmission failure count is at least 1
-    try {
         expect(transmissionFailureCount).to.be.at.least(1);
         console.log('Test case passed: At least one transmission failure detected.');
-    } catch (error) {
-        console.error('Test case failed: No transmission failure detected.');
-        process.exit(1); // Exit with a non-zero exit code to indicate failure
-    }
+   
 
     console.log('All is well (maybe!)');
-}).catch(error => {
-    console.error('An error occurred:', error);
-    process.exit(1); // Exit with a non-zero exit code to indicate failure
-});
+
 
