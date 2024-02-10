25c901a97a7439a2146d1b3e0990ee3e25f61c86
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-10 15:32:57 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -3,8 +3,6 @@ const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
 
 function print_color_map() {
     let color_map = "";
-    color_map += "Index | Major Color | Minor Color\n"; // Adding header
-    color_map += "--------------------------------\n"; // Adding separator
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
             color_map += `${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}\n`;
