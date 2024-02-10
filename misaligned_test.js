af25937e23ecfad758ca0e22de1025feaa257cbb
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-10 15:53:29 +0530

    Update misaligned_test.js

--- misaligned_test.js
+++ misaligned_test.js
@@ -1,14 +1,12 @@
 const { print_color_map } = require('./misaligned.js');
 const { expect } = require("chai");
-
+const returned_contents_print_color_map = print_color_map();
+const colorMap = returned_contents_print_color_map.color_map || returned_contents_print_color_map[0]; // Handle direct return or return via object
+const majorColors = returned_contents_print_color_map.majorColors || returned_contents_print_color_map[1]; // Handle direct return or return via object
+const minorColors = returned_contents_print_color_map.minorColors || returned_contents_print_color_map[2]; // Handle direct return or return via object
 
     // The size of misaligned table is compared with actual table.When table is misaligned,it fails at this test case
  function testColorMapLength() {
-    const returned_contents_print_color_map = print_color_map();
-    const colorMap = returned_contents_print_color_map.color_map || returned_contents_print_color_map[0]; // Handle direct return or return via object
-    const majorColors = returned_contents_print_color_map.majorColors || returned_contents_print_color_map[1]; // Handle direct return or return via object
-    const minorColors = returned_contents_print_color_map.minorColors || returned_contents_print_color_map[2]; // Handle direct return or return via object
-
     const colorMapLines = colorMap.split("\n").filter(Boolean);
     const majorColorArray = [...majorColors];
     const minorColorArray = [...minorColors];
@@ -20,8 +18,7 @@ const { expect } = require("chai");
     //The misalignment is identified by the column width occupied,when output is not aligned it has different column width at each row
     function testColumnAlignment() {
         
-            const result = print_color_map();
-            const rows = result.trim().split('\n');
+            const rows = colorMap.trim().split('\n');
             const columns = rows.map(row => row.split('|').map(cell => cell.trim()));
 
             // Check alignment of each column
