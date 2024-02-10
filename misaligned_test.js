46411a497a8f1a81d7bfacec3a8bde389586c265
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-10 12:17:38 +0530

    Update misaligned_test.js
    comments are added

--- misaligned_test.js
+++ misaligned_test.js
@@ -4,13 +4,15 @@ import('chai').then(chai => {
 
 
     const { print_color_map, majorColors, minorColors } = require('./misaligned.js');
-
+    
+    // The size of misaligned table is compared with actual table.When table is misaligned,it fails at this test case
     function testColorMapLength() {
         const result = print_color_map();
         const colorMapLength = result.length;
+        //This fails
         expect(colorMapLength).to.equal(majorColors * minorColors);
     }
-
+    //The misalignment is identified by the column width occupied,when output is not aligned it has different column width at each row
     function testColumnAlignment() {
         const result = print_color_map();
         const rows = result.trim().split('\n');
@@ -19,6 +21,7 @@ import('chai').then(chai => {
         // Check alignment of each column
         columns.forEach(column => {
             const columnWidth = Math.max(...column.map(cell => cell.length));
+            //This fails
             expect(column.every(cell => cell.length === columnWidth)).to.be.true;
         });
     }
