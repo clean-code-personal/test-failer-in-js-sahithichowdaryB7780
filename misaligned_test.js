f366df350f33c9ccde0649f863a5d3a75f5b6308
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-10 23:14:05 +0530

    Update misaligned_test.js

--- misaligned_test.js
+++ misaligned_test.js
@@ -1,25 +1,28 @@
 const { print_color_map } = require('./misaligned.js');
 const { expect } = require("chai");
 const returned_contents_print_color_map = print_color_map();
-const colorMap = returned_contents_print_color_map.color_map ; // Handle direct return or return via object
-const majorColors = returned_contents_print_color_map.majorColors ; // Handle direct return or return via object
-const minorColors = returned_contents_print_color_map.minorColors; // Handle direct return or return via object
+const colorMap = returned_contents_print_color_map.color_map ; 
+const majorColors = returned_contents_print_color_map.majorColors ; 
+const minorColors = returned_contents_print_color_map.minorColors; 
 console.log(colorMap);
  //The misalignment is identified by the column width occupied,when output is not aligned it has different column width at each row
 function testColumnAlignment() {
-        
+            //an array is formed with each row as an element in array
             const rows = colorMap.trim().split('\n');
+            //multi-array is formed with each cell acts as elements inside inner array
             const columns = rows.map(row => row.split('|').map(cell => cell.trim()));
 
             // Check alignment of each column
             columns.forEach(column => {
+                 //We calculate the maximum cell length
                 const columnWidth = Math.max(...column.map(cell => cell.length));
-                //This fails
+                //This fails as we compare all cell lengths are equal or not
                 expect(column.every(cell => cell.length === columnWidth)).to.be.true;
             });
         } 
 //The misalignment is identified by the column width occupied,when output is not aligned it has different column width at each row
  function testColorMapLength() {
+    //colorMap split at \n character and we filter to delete empty rows
     const colorMapLines = colorMap.split("\n").filter(Boolean);
     const maxSizeOfColorPair = (majorColors.length * minorColors.length);
     console.log(colorMapLines.length);
