b58943604b62000e7db160a97137af3b65240c0b
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-10 15:22:00 +0530

    Update tshirts_test.js

--- tshirts_test.js
+++ tshirts_test.js
@@ -1,17 +1,11 @@
 const { size } = require('./tshirts');
-
-import('chai').then(chai => {
-    const { expect } = chai;
-
+const { size } = require("../tshirts.js");
     expect(size(37)).to.equal('S');
     expect(size(40)).to.equal('M');
     expect(size(43)).to.equal('L');
     // The expected output is 'S',but actual output is 'L'.The test case fails
-    expect(size(38)).to.equal('S');  
+    expect(size(38)).to.equal('S');
+    console.log("All is well (maybe!)");
 
-    console.log('All is well (maybe!)');
-}).catch(error => {
-    console.error('An error occurred:', error);
-    process.exit(1); // Exit with a non-zero exit code to indicate failure
-});
+  
 
