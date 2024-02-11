a0d3908c38928c3835405521815269e7fea3edcd
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-12 00:30:15 +0530

    Update transmitter.js

--- transmitter.js
+++ transmitter.js
@@ -10,8 +10,5 @@ function transmitInCelcius(fahrenheit, networkTransmit = networkTransmitStub) {
   }
 }
 
-let newTransmissionFailureCount=transmissionFailureCount;
-
-
-module.exports = { transmitInCelcius, transmissionFailureCount, newTransmissionFailureCount };
+module.exports = { transmitInCelcius, transmissionFailureCount };
 
