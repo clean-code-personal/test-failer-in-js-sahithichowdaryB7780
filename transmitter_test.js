7daaf1d5c92bfa8d01ad9f5ceec62531700afde4
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-12 01:18:30 +0530

    Update transmitter_test.js

--- transmitter_test.js
+++ transmitter_test.js
@@ -1,27 +1,39 @@
-const { transmitInCelcius, transmissionFailureCount,b } = require('./transmitter');
+const { transmitInCelcius,transmissionFailureCount } = require('./transmitter');
 const { expect }=require('chai');
 function networkTransmitStub(celsius) {
     console.log(`Temperature to transmit: ${celsius} Celsius`);
     // Generate a random number between 0 and 1
-    const randomNumber = Math.random();
+    const randomNumber = Math.round(Math.random());
     // Map the random number to either 200 or 500
     // If randomNumber is less than 0.5, return 200, otherwise return 500
-    return randomNumber < 0.5 ? 200 : 500;
-  }
-    //transmit temperatures
-function transmissionFailureCountTest(networkTransmitStub){
-    transmitInCelcius(410.4,networkTransmitStub);
-    transmitInCelcius(45.5,networkTransmitStub);
-    transmitInCelcius(499,networkTransmitStub);
-}
-transmissionFailureCountTest();
+    return randomNumber == 0 ? 200 : 500;
+       // The intention here is to transmit the temperature over a network.
+    // However, this is a stub, so we just print the message above
+    // and give back a return code.
 
+}
+// Transmit temperatures
+transmitInCelcius(99.01234567901234, networkTransmitStub);
+console.log('transmissionFailureCount:',transmissionFailureCount);
+
+
+transmitInCelcius(-13.61111111111111, networkTransmitStub);
+console.log('transmissionFailureCount:', transmissionFailureCount);
+
+
+transmitInCelcius(126.35802469135803, networkTransmitStub);
+console.log('transmissionFailureCount:',transmissionFailureCount);
+
+
+transmitInCelcius(126.66666666666667, networkTransmitStub);
+console.log('transmissionFailureCount:',transmissionFailureCount);
 
 // Log transmission failure count
-console.log(`Transmission failed ${b} times.`);
+console.log(`Transmission failed ${transmissionFailureCount} times.`);
 
 // Test if transmission failure count is at least 1
-expect(b).to.be.at.least(1);
+expect(transmissionFailureCount).to.be.at.least(1);
 console.log('Test case passed: At least one transmission failure detected.');
 
 console.log('All is well (maybe!)');
+
