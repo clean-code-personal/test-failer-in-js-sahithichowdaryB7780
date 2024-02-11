ef92ad883d10994d8b2823b02812d71b98118b79
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-12 01:16:55 +0530

    Update transmitter.js

--- transmitter.js
+++ transmitter.js
@@ -1,16 +1,14 @@
-let transmissionFailureCount = 0;
-let b=0;
-function transmitInCelcius(fahrenheit,networkTransmit = networkTransmitStub) {
-    const celsius = (fahrenheit - 32) * 5 / 9;
-    const returnCode = networkTransmit(celsius);
-    if (returnCode !== 200) {
-        // non-ok response indicates failure while transmitting over the network
-        // record of failure count
-        transmissionFailureCount += 1;
-        console.log('transmissionFailureCount in trans:',transmissionFailureCount);
-    }
-    b=transmissionFailureCount;
+transmitter.js:let transmissionFailureCount=0;
+
+function transmitInCelcius(fahrenheit, networkTransmit = networkTransmitStub) {
+  const celsius = (fahrenheit - 32) * 5 / 9;
+  const returnCode = networkTransmit(celsius);
+  if (returnCode !== 200) {
+    // Increment failure count for non-200 return codes
+    transmissionFailureCount ++;
+    console.log('transmissionFailureCount in transmitter.js:',transmissionFailureCount);
+  }
 }
 
-module.exports = { transmitInCelcius, transmissionFailureCount ,b };
+module.exports = { transmitInCelcius, transmissionFailureCount };
 
