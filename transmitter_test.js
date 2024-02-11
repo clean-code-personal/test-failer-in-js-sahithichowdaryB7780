840c05af8864e569d11920c813cd20deef889d54
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-12 00:39:21 +0530

    Update transmitter_test.js

--- transmitter_test.js
+++ transmitter_test.js
@@ -1,5 +1,6 @@
-const { transmitInCelcius,transmissionFailureCount } = require('./transmitter');
-const { expect }=require('chai');
+const { transmitInCelcius, incrementTransmissionFailureCount, transmissionFailureCount } = require('./transmitter');
+const { expect } = require('chai');
+
 function networkTransmitStub(celsius) {
     console.log(`Temperature to transmit: ${celsius} Celsius`);
     // Generate a random number between 0 and 1
@@ -7,26 +8,32 @@ function networkTransmitStub(celsius) {
     // Map the random number to either 200 or 500
     // If randomNumber is less than 0.5, return 200, otherwise return 500
     return randomNumber == 0 ? 200 : 500;
-       // The intention here is to transmit the temperature over a network.
-    // However, this is a stub, so we just print the message above
-    // and give back a return code.
-
 }
-// Transmit temperatures
-transmitInCelcius(99.01234567901234, networkTransmitStub);
-console.log('transmissionFailureCount:',transmissionFailureCount);
 
-
-transmitInCelcius(-13.61111111111111, networkTransmitStub);
+// Modify transmitInCelcius to use shared state
+transmitInCelcius.bind({ incrementTransmissionFailureCount })(
+  99.01234567901234,
+  networkTransmitStub
+);
 console.log('transmissionFailureCount:', transmissionFailureCount);
 
+transmitInCelcius.bind({ incrementTransmissionFailureCount })(
+  -13.61111111111111,
+  networkTransmitStub
+);
+console.log('transmissionFailureCount:', transmissionFailureCount);
 
-transmitInCelcius(126.35802469135803, networkTransmitStub);
-console.log('transmissionFailureCount:',transmissionFailureCount);
+transmitInCelcius.bind({ incrementTransmissionFailureCount })(
+  126.35802469135803,
+  networkTransmitStub
+);
+console.log('transmissionFailureCount:', transmissionFailureCount);
 
-
-transmitInCelcius(126.66666666666667, networkTransmitStub);
-console.log('transmissionFailureCount:',transmissionFailureCount);
+transmitInCelcius.bind({ incrementTransmissionFailureCount })(
+  126.66666666666667,
+  networkTransmitStub
+);
+console.log('transmissionFailureCount:', transmissionFailureCount);
 
 // Log transmission failure count
 console.log(`Transmission failed ${transmissionFailureCount} times.`);
@@ -36,6 +43,9 @@ expect(transmissionFailureCount).to.be.at.least(1);
 console.log('Test case passed: At least one transmission failure detected.');
 
 console.log('All is well (maybe!)');
+
+
+
 
 
 
