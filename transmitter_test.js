b8697df37286aa42a0cf66816506f40ee18e316c
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-11 22:12:26 +0530

    Update transmitter_test.js

--- transmitter_test.js
+++ transmitter_test.js
@@ -1,30 +1,39 @@
 const { transmitInCelcius, transmissionFailureCount } = require('./transmitter');
 const { expect } = require('chai');
 
-function networkTransmitStub(celsius) {
+function networkTransmitStub(celsius, callback) {
     console.log(`Temperature to transmit: ${celsius} Celsius`);
     const randomNumber = Math.random();
-    return Promise.resolve(randomNumber < 0.5 ? 200 : 500);
+    const returnCode = randomNumber < 0.5 ? 200 : 500;
+    callback(returnCode);
 }
 
-async function runTests() {
+function runTests() {
+    let transmissionsCompleted = 0;
+
+    function transmissionCallback() {
+        transmissionsCompleted++;
+        if (transmissionsCompleted === 4) { // 4 transmissions in total
+            // Log transmission failure count
+            console.log(`Transmission failed ${transmissionFailureCount} times.`);
+            
+            // Test if transmission failure count is at least 1
+            expect(transmissionFailureCount).to.be.at.least(1);
+            console.log('Test case passed: At least one transmission failure detected.');
+
+            console.log('All is well (maybe!)');
+        }
+    }
+
     // Transmit temperatures
-    await transmitInCelcius(410.4, networkTransmitStub);
-    await transmitInCelcius(45.5, networkTransmitStub);
-    await transmitInCelcius(499, networkTransmitStub);
-    await transmitInCelcius(500, networkTransmitStub);
-
-    // Log transmission failure count
-    console.log(`Transmission failed ${transmissionFailureCount} times.`);
-
-    // Test if transmission failure count is at least 1
-    expect(transmissionFailureCount).to.be.at.least(1);
-    console.log('Test case passed: At least one transmission failure detected.');
-   
-    console.log('All is well (maybe!)');
+    transmitInCelcius(410.4, networkTransmitStub.bind(null, 410.4, transmissionCallback));
+    transmitInCelcius(45.5, networkTransmitStub.bind(null, 45.5, transmissionCallback));
+    transmitInCelcius(499, networkTransmitStub.bind(null, 499, transmissionCallback));
+    transmitInCelcius(500, networkTransmitStub.bind(null, 500, transmissionCallback));
 }
 
 runTests();
+
 
 
 
