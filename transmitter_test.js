01ae8c5d8b6e20970bd5abab0f3be0fd62a64f7e
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-11 21:59:41 +0530

    Update transmitter_test.js

--- transmitter_test.js
+++ transmitter_test.js
@@ -1,11 +1,22 @@
 const { transmitInCelcius, transmissionFailureCount } = require('./transmitter');
 const { expect }=require('chai');
+function networkTransmitStub(celsius) {
+    console.log(`Temperature to transmit: ${celsius} Celsius`);
+    // Generate a random number between 0 and 1
+    const randomNumber = Math.round(Math.random());
+    // Map the random number to either 200 or 500
+    // If randomNumber is less than 0.5, return 200, otherwise return 500
+    return randomNumber < 0.5 ? 200 : 500;
+    // The intention here is to transmit the temperature over a network.
+    // However, this is a stub, so we just print the message above
+    // and give back a return code.
+}
 
     //transmit temperatures
-    transmitInCelcius(410.4);
-    transmitInCelcius(45.5);
-    transmitInCelcius(499);
-    transmitInCelcius(500);
+    transmitInCelcius(410.4,networkTransmitStub);
+    transmitInCelcius(45.5,networkTransmitStub);
+    transmitInCelcius(499,networkTransmitStub);
+    transmitInCelcius(500,networkTransmitStub);
 
     // Log transmission failure count
     console.log(`Transmission failed ${transmissionFailureCount} times.`);
