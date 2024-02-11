ee20fb51e68e23ecdf99d0c6e2eb4d8f0ce14e94
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-11 22:08:51 +0530

    Update transmitter_test.js

--- transmitter_test.js
+++ transmitter_test.js
@@ -1,36 +1,30 @@
-
 const { transmitInCelcius, transmissionFailureCount } = require('./transmitter');
 const { expect } = require('chai');
 
 function networkTransmitStub(celsius) {
     console.log(`Temperature to transmit: ${celsius} Celsius`);
     const randomNumber = Math.random();
-    return randomNumber < 0.5 ? 200 : 500;
+    return Promise.resolve(randomNumber < 0.5 ? 200 : 500);
 }
 
-function runTests() {
-    // Transmit temperature 1
-    transmitInCelcius(410.4, function() {
-        // Transmit temperature 2
-        transmitInCelcius(45.5, function() {
-            // Transmit temperature 3
-            transmitInCelcius(499, function() {
-                // Transmit temperature 4
-                transmitInCelcius(500, function() {
-                    // Log transmission failure count
-                    console.log(`Transmission failed ${transmissionFailureCount} times.`);
+async function runTests() {
+    // Transmit temperatures
+    await transmitInCelcius(410.4, networkTransmitStub);
+    await transmitInCelcius(45.5, networkTransmitStub);
+    await transmitInCelcius(499, networkTransmitStub);
+    await transmitInCelcius(500, networkTransmitStub);
 
-                    // Test if transmission failure count is at least 1
-                    expect(transmissionFailureCount).to.be.at.least(1);
-                    console.log('Test case passed: At least one transmission failure detected.');
-                   
-                    console.log('All is well (maybe!)');
-                });
-            });
-        });
-    });
+    // Log transmission failure count
+    console.log(`Transmission failed ${transmissionFailureCount} times.`);
+
+    // Test if transmission failure count is at least 1
+    expect(transmissionFailureCount).to.be.at.least(1);
+    console.log('Test case passed: At least one transmission failure detected.');
+   
+    console.log('All is well (maybe!)');
 }
 
 runTests();
+
 
 
