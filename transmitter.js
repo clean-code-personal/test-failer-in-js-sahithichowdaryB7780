17391ebb30eac7905ae84117f90bb277070ccc14
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-11 23:23:39 +0530

    Update transmitter.js

--- transmitter.js
+++ transmitter.js
@@ -3,8 +3,10 @@ let transmissionFailureCount = 0;
 function transmitInCelcius(fahrenheit, networkTransmit = networkTransmitStub) {
   const celsius = (fahrenheit - 32) * 5 / 9;
   const returnCode = networkTransmit(celsius);
+  console.log("xx");
   if (returnCode !== 200) {
     // Increment failure count for non-200 return codes
+    console.log("hi");
     transmissionFailureCount += 1;
   }
 }
