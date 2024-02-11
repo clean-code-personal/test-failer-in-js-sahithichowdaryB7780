1edcbda2c8313d157e3c8506f786ee7665fe6bba
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-12 00:42:48 +0530

    Update transmitter.js

--- transmitter.js
+++ transmitter.js
@@ -1,11 +1,11 @@
 let transmissionFailureCount = 0;
 
-function transmitInCelcius(fahrenheit, networkTransmit = networkTransmitStub) {
+function transmitInCelcius(fahrenheit, networkTransmit = networkTransmitStub.bind(this)) {
   const celsius = (fahrenheit - 32) * 5 / 9;
   const returnCode = networkTransmit(celsius);
   if (returnCode !== 200) {
     // Increment failure count for non-200 return codes
-    this.incrementTransmissionFailureCount();
+    incrementTransmissionFailureCount();
     console.log('transmissionFailureCount in transmitter.js:', transmissionFailureCount);
   }
 }
@@ -14,7 +14,14 @@ function incrementTransmissionFailureCount() {
   transmissionFailureCount++;
 }
 
-module.exports = { transmitInCelcius, incrementTransmissionFailureCount, transmissionFailureCount };
+function networkTransmitStub(celsius) {
+    console.log(`Temperature to transmit: ${celsius} Celsius`);
+    // Generate a random number between 0 and 1
+    const randomNumber = Math.round(Math.random());
+    // Map the random number to either 200 or 500
+    // If randomNumber is less than 0.5, return 200, otherwise return 500
+    return randomNumber == 0 ? 200 : 500;
+}
 
-
+module.exports = { transmitInCelcius, transmissionFailureCount };
 
