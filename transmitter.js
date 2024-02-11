c6fb1e607001e6b6ef72079ca312d4d3cdf765f7
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-12 01:14:38 +0530

    Update transmitter.js

--- transmitter.js
+++ transmitter.js
@@ -1,8 +1,8 @@
 let transmissionFailureCount = 0;
 let b=0;
-function transmitInCelcius(fahrenheit,networkTransmitStub) {
+function transmitInCelcius(fahrenheit,networkTransmit = networkTransmitStub) {
     const celsius = (fahrenheit - 32) * 5 / 9;
-    const returnCode = networkTransmitStub(celsius);
+    const returnCode = networkTransmit(celsius);
     if (returnCode !== 200) {
         // non-ok response indicates failure while transmitting over the network
         // record of failure count
