01b599ed1c81f5c0591b21ae2ae91248574b4f09
sahithichowdaryB7780 <158990777+sahithichowdaryB7780@users.noreply.github.com>
2024-02-12 16:52:59 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -3,17 +3,21 @@ const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
 
 function print_color_map() {
   let color_map = "";
+  //max length of index is calculated
   const maxIndexWidth = String(majorColors.length * minorColors.length).length;
+  //max length of majorcolor is calculated
   const maxMajorColorsWidth = Math.max(...majorColors.map(color => color.length));
+  //max length of minorcolor is calculated
   const maxMinorColorsWidth = Math.max(...minorColors.map(color => color.length));
 
   for (let i = 0; i < majorColors.length; i++) {
     for (let j = 0; j < minorColors.length; j++) {
       const index = i * minorColors.length + j + 1;
+      // we are fitting the element in max widths possible
       const paddedIndex = String(index).padStart(maxIndexWidth);
       const paddedMajorColor = String(majorColors[i]).padEnd(maxMajorColorsWidth, " "); // Use spaces for padding
       const paddedMinorColor = String(minorColors[j]).padEnd(maxMinorColorsWidth, " "); // Use spaces for padding
-
+      // formatting the string
       color_map += `${paddedIndex}|${paddedMajorColor}|${paddedMinorColor}\n`;
     }
   }
